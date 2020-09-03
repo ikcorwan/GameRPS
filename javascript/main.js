c1a915c196ffae03cc17e4aa4a395090f8245ab6
Jarosław Nawrocki <nawrockij@o2.pl>
2020-09-03 16:59:38 +0200

    Fix display of units on mobile phone

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,15 @@
+// *** fix display of units on mobile phone ***
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+// *** Game ***
 const gameSummary = {
     numbers: 0,
     wins: 0,
